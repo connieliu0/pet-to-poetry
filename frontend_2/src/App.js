473a5145eeb2e881f890cb894b074fc89f294829
Bo Hu <bh456@cornell.edu>
2020-11-30 22:55:15 -0500

    connected YOLO to frontend

--- frontend_2/src/App.js
+++ frontend_2/src/App.js
@@ -1,15 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import FastAverageColor from 'fast-average-color';
-import P5Wrapper from 'react-p5-wrapper';
-import ml5 from 'ml5';
 import './App.css';
 
 function App() {
   const fac = new FastAverageColor()
-  // a list of image names
-  // const [img, setImg] = useState({});
   const [src, setSrc] = useState("");
-  const [temp, setTemp] = useState(0);
+  const [noun1, setNoun1] = useState("");
+  const [noun2, setNoun2] = useState("");
+  const [verb, setVerb] = useState("");
+  const [structure, setStructure] = useState("");
+  const [poem, setPoem] = useState("");
+  var newstring = "";
+  const happyNouns = ["Sun", "Cloud", "Star", "Hope", "Laughter", "Dream", "Victory", "Joy"
+  ]
+  const happyVerbs = ["Soar", "Spring", "Fly", "Skip"
+  ]
+  const sadNouns = ["Ocean", "Thunder", "Darkness", "Moon", "Crater", "Despair"
+  ]
+  const sadVerbs = ["Sludge", "Slog", "Drown", "Drag"
+  ]
+  const structures = [
+    "I miss noun1, hidden away in noun2’s warm light\n\nThe noun1 verbs and verbs",
+    "noun1 sleeps, noun2 verbs\n\nverb well, noun1",
+    "I see noun1, above the noun2. noun1 visits noun2 happily, verbing",
+    "noun1 runs, verbs to the noun2\n\nnoun1 where are you verbing?\n\nverb away, noun1."
+  ]
 
   const readURL = file => {
     // Check if the file is an image.
@@ -31,70 +46,71 @@ function App() {
   const changeImg = e => {
     console.log(e.target.files);
     console.log(e.target.files[0]);
-    console.log(e.target.files[1])
     const i = e.target.files[0];
     // setImg(e.target.files[0]);
     // console.log(img);
     readURL(i);
   }
 
+  const UseYolo = src => {
+    // with users' uploaded data
+    // fetch(`/parseImages/${src}`).then(res => res.json()).then(data => {
+    //   console.log(data);
+    // })
+    fetch('/parseImages').then(res => res.json()).then(data => {
+      console.log(data.resp2);
+    })
+  }
+
   const upload = () => {
     const img = document.getElementById("output");
+    // console.log(img.src);
+    UseYolo(img.src);
     const color = fac.getColor(img);
     console.log(color);
-    calcTemp(color.isDark);
+    setNounVerb(color.isDark);
+    setPoemStructure(color.value);
+    makePoem();
   }
 
-  const calcTemp = isDark => {
-    if (isDark) setTemp(1);
-    setTemp(0.33);
-  }
-
-  let lstm;
-  const [res, setRes] = useState("res");
-  const [text, setText] = useState("the meaning of life is...");
-  const [length, setLength] = useState(100);
-  const [status, setStatus] = useState("Model Loading");
-
-
-  const sketch = p => {
-    console.log("running sketch");
-
-    p.setup = function () {
-      console.log("p.setup running");
-      p.noCanvas();
-
-      // Create the LSTM Generator passing it the model directory
-      lstm = ml5.charRNN('./data/', p.modelReady());
-      console.log("lstm", lstm)
-    };
-    p.modelReady = () => {
-      console.log("modelReady running");
-      setStatus("Model Loaded");
-    };
-
-  };
-  // Generate new text
-  const generate = () => {
-    console.log("generate running");
-
-    if (text.length > 0) {
-      // Seed text, temperature, length to outputs
-      let data = {
-        seed: text,
-        temperature: temp,
-        length: length
-      };
-
-      // Generate text with the lstm
-      lstm.generate(data, gotData());
-
-      function gotData(err, result) {
-        console.log(result);
-        console.log("got data");
-        setRes(res + result);
-      }
+  const setPoemStructure = value => {
+    var total = value[0] + value[1] + value[2]
+    if (total < 192) {
+      setStructure(structures[0]);
     }
+    else if (total < 384) {
+      setStructure(structures[1]);
+    }
+    else if (total < 576) {
+      setStructure(structures[2]);
+
+    }
+    else if (total < 765) {
+      setStructure(structures[3]);
+    }
+  }
+
+  const setNounVerb = isDark => {
+    if (isDark) {
+      setNoun1(sadNouns[Math.floor(Math.random() * sadNouns.length)]);
+      setNoun2(sadNouns[Math.floor(Math.random() * sadNouns.length)]);
+      setVerb(sadVerbs[Math.floor(Math.random() * sadVerbs.length)]);
+    }
+    else {
+      setNoun1(happyNouns[Math.floor(Math.random() * happyNouns.length)]);
+      setNoun2(happyNouns[Math.floor(Math.random() * happyNouns.length)]);
+      setVerb(happyVerbs[Math.floor(Math.random() * happyVerbs.length)]);
+    }
+  }
+  const makePoem = () => {
+    console.log(noun1);
+    console.log(noun2);
+    console.log(verb);
+
+    newstring = structure.replaceAll("noun1", noun1);
+    newstring = newstring.replaceAll("noun2", noun2);
+    newstring = newstring.replaceAll("verb", verb);
+    setPoem(newstring);
   }
 
   return (
@@ -110,23 +126,9 @@ function App() {
       <br />
       <button id="upload" onClick={upload}>upload</button>
       {/* </form> */}
-      <P5Wrapper sketch={sketch} />
-      <div id="">
-        <h1> LSTM Text Generation Example</h1>
-        <h2> This example uses a pre-trained model on a corpus of Emily Dickinson</h2>
-        <div className="example">
-          <p>seed text: <input id="textInput" value={text} onChange={e => setText(e.target.value)} /></p>
-          <p>length:
-            <input id="lengthSlider" min="10" max="500" value={length} type="range" onChange={e => setLength(e.target.value)} />
-            <span id="length">{length}</span>
-          </p>
-          <button id="generate" onClick={generate}>generate</button>
-          <p id="status">{status}</p>
-          <p id="result">{res}</p>
-        </div>
-      </div>
+      {/* <button id="generate" onClick={generate}>generate</button> */}
+      <p id="result">{poem}</p>
     </div>
-
   );
 }
 
