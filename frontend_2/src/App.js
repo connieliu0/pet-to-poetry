eb6455821e991a6cb372448cac798e6687a81896
Bo Hu <bh456@cornell.edu>
2020-12-13 09:54:00 -0500

    connected to poetryDB

--- frontend_2/src/App.js
+++ frontend_2/src/App.js
@@ -2,20 +2,26 @@ import React, { useState, useEffect } from 'react';
 import FastAverageColor from 'fast-average-color';
 import './App.css';
 const { HostedModel } = require('@runwayml/hosted-models');
+const unirest = require('unirest');
 
 function App() {
   const fac = new FastAverageColor()
   const [src, setSrc] = useState("");
+  const [filename, setFilename] = useState("");
   const [noun1, setNoun1] = useState("");
   const [noun2, setNoun2] = useState("");
   const [verb, setVerb] = useState("");
   const [structure, setStructure] = useState("");
   const [poem, setPoem] = useState("");
+  const [yolo, setYolo] = useState([]);
   const [generated_text, setText] = useState("");
+  const [author, setAuthor] = useState("");
+  const [title, setTitle] = useState("");
+  const [result, setResult] = useState([]);
   var newstring = "";
   const happyNouns = ["sun", "cloud", "star", "flower"
   ]
-  const happyVerbs = ["soar", "spring", "jump", 
+  const happyVerbs = ["soar", "spring", "jump",
   ]
   const sadNouns = ["ocean", "thunder", "darkness", "moon", "snake"
   ]
@@ -28,8 +34,35 @@ function App() {
     "The noun1 verbs, verbs to the noun2."
   ]
 
+  const searchLines = (poem, term) => {
+    const lines = poem.lines;
+    const reducer = (acc, line) => acc || line.includes(term);
+    return lines.reduce(reducer, false)
+  }
+
+
+  // TODO: fix the YOLO img sending issues, 
+  // sort the sad poems
+  const getShortestSadPoetry = 0
+
+  const getPoetry = (term, isHappy) => {
+    const url = isHappy ?
+      `https://poetrydb.org/lines,linecount/${term};14` :
+      `https://poetrydb.org/lines/${term}`
+    unirest.get(url).then(res => {
+      const body = res.body;
+      // add a try block
+      console.log(body);
+      const real = body.filter(poem => searchLines(poem, term));
+      console.log(real);
+      setAuthor(real[0].author);
+      setTitle(real[0].title);
+      setResult(real[0].lines);
+    })
+  }
+
   const readURL = file => {
-    // Check if the file is an image.
+    console.log(file);
     if (file.type && file.type.indexOf('image') === -1) {
       console.log('File is not an image.', file.type, file);
       return;
@@ -38,42 +71,78 @@ function App() {
     const reader = new FileReader();
     reader.addEventListener('load', (event) => {
       setSrc(event.target.result);
-      // const img = document.getElementById("output");
-      // const color = fac.getColor(img);
-      // console.log(color);
     });
     reader.readAsDataURL(file);
   }
 
   const changeImg = e => {
-    console.log(e.target.files);
-    console.log(e.target.files[0]);
     const i = e.target.files[0];
-    // setImg(e.target.files[0]);
-    // console.log(img);
-    readURL(i);
+    setFilename(i.name);
+    console.log(i);
+    // readURL(i);
+    const url = URL.createObjectURL(i);
+    setSrc(url);
   }
 
-  // const UseYolo = src => {
-  //   // with users' uploaded data
-  //   // fetch(`/parseImages/${src}`).then(res => res.json()).then(data => {
-  //   //   console.log(data);
-  //   // })
-  //   fetch('/parseImages').then(res => res.json()).then(data => {
-  //     console.log(data.resp2);
-  //     const noun=data.resp2;
-  //   })
-  // }
+  const compare = (a, b) => {
+    const idx1 = a.indexOf(' ');
+    const idx2 = b.indexOf(' ');
+    const num1 = parseInt(a.substr(idx1 + 1));
+    const num2 = parseInt(b.substr(idx2 + 1));
+    if (num1 < num2) {
+      return 1;
+    } else if (num1 > num2) {
+      return -1;
+    } else {
+      return 0;
+    }
+  }
+
+  const sortYoloData = res => {
+    const sorted = res.sort(compare);
+    const strings = sorted.map((r, i) => {
+      const idx = r.indexOf(' ');
+      return r.substr(0, idx);
+    })
+    return strings
+  }
+
+  const UseYolo = (data, isHappy) => {
+    const formData = new FormData();
+    formData.append("file", src);
+
+    fetch(`/parseImages?img=${src}&filename=downloaded`,
+      // {
+      //   method: "POST",
+      //   body: data,
+      //   headers: { 'Content-Type': 'multipart/form-data' }
+      // }
+    )
+      .then(res => res.json())
+      .then(data => {
+        const res = data.yoloRes
+        const sorted = sortYoloData(res);
+        console.log(sorted);
+        setYolo(sorted);
+        const term = " " + sorted[0] + " "
+        console.log(term);
+        getPoetry(term, isHappy);
+      })
+  }
 
   const upload = () => {
     const img = document.getElementById("output");
-    // console.log(img.src);
-    // UseYolo(img.src);
     const color = fac.getColor(img);
-    console.log(color);
-    setNounVerb(color.isDark);
-    setPoemStructure(color.value);
-    makePoem();
+    // const formData = new FormData();
+    // formData.append("file", src);
+    // formData.append("filename", "downloaded");
+    // console.log(formData);
+    // UseYolo(formData, color.isLight);
+    UseYolo(color.isLight);
+    // original implementation
+    // setNounVerb(color.isDark);
+    // setPoemStructure(color.value);
+    // makePoem();
   }
 
   const setPoemStructure = value => {
@@ -96,12 +165,12 @@ function App() {
   const setNounVerb = isDark => {
     if (isDark) {
       setNoun1(sadNouns[Math.floor(Math.random() * sadNouns.length)]);
-      setNoun2(sadNouns[Math.floor(Math.random() * sadNouns.length)]);
+      setNoun2(yolo[Math.floor(Math.random() * sadNouns.length)]);
       setVerb(sadVerbs[Math.floor(Math.random() * sadVerbs.length)]);
     }
     else {
       setNoun1(happyNouns[Math.floor(Math.random() * happyNouns.length)]);
-      setNoun2(happyNouns[Math.floor(Math.random() * happyNouns.length)]);
+      setNoun2(yolo[Math.floor(Math.random() * happyNouns.length)]);
       setVerb(happyVerbs[Math.floor(Math.random() * happyVerbs.length)]);
     }
   }
@@ -121,25 +190,25 @@ function App() {
   });
   //// You can use the info() method to see what type of input object the model expects
   // model.info().then(info => console.log(info));
-  const makeGenerated=()=>{
-  const inputs = {
-    "input_prompt": poem,
-    "length": 20,
-    "temperature": .6,
-    "top_p": .7
-  };
+  const makeGenerated = () => {
+    const inputs = {
+      "input_prompt": poem,
+      "length": 20,
+      "temperature": .6,
+      "top_p": .7
+    };
 
-  model.query(inputs).then(outputs => {
-    setText(outputs.generated_text);
-    // use the outputs in your project
-    console.log(outputs.generated_text);
-  });
-};
+    model.query(inputs).then(outputs => {
+      setText(outputs.generated_text);
+      // use the outputs in your project
+      console.log(outputs.generated_text);
+    });
+  };
   return (
     <div className="App">
-      {/* <form> */}
-      <div id="prompt">Upload your images:</div>
+      <div id="prompt">Upload a picture of your pet:</div>
       <br />
+      {/* <form onSubmit={upload} encType="multipart/form-data"> */}
       <input type="file" id="chooseImg" name="files" onChange={changeImg} />
       <br />
       {src !== "" &&
@@ -148,10 +217,14 @@ function App() {
       <br />
       <button id="upload" onClick={upload}>upload</button>
       {/* </form> */}
-      {/* <button id="generate" onClick={generate}>generate</button> */}
       <p id="result">{poem}</p>
-      <button id="generate" onClick={makeGenerated}>generate</button>
-      <p id="result">{generated_text}</p>
+      {/* original implementation */}
+      {/* <button id="generate" onClick={makeGenerated}>generate</button> */}
+      {/* <p id="result">{generated_text}</p> */}
+      {title === "" ? <p></p> : <p id="title">{title}</p>}
+      {author === "" ? <p></p> : <p id="author">{author}</p>}
+      {result === [] ? <p></p> : result.map((r, i) =>
+        (<p className="poetryLine" key={i}>{r}</p>))}
     </div>
   );
 }
